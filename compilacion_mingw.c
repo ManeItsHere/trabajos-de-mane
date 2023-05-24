87b7d876f5d39f8c1ef85b14736d280bcc7c2493
Mané <131938509+ManeItsHere@users.noreply.github.com>
2023-05-24 07:23:32 -0600

    Add files via upload

---
+++ compilacion_mingw.c
@@ -0,0 +1,8 @@
+/*   COMPILACIÓN EN MINGW
+1.-Descargar el instalador de MinGW desde el sitio oficial
+2.-Al finalizar se le agrega el directorio "bin" en el entorno path
+3.- se abre el símbolo del sistema (cmd)
+4.-verificar que MinGW se haya instalado correctamente, escribiendo gcc__ versión en línea de comando
+5.-compila el programa utilizando el comando gcc , seguido del nombre del archivo
+6.-Si no hay errores de compilación, se genera el archivo ejecutablecon el nombre especificado
+ */
