4d9e4d39df813858d3a21a043c34ee075b97011c
Mané <131938509+ManeItsHere@users.noreply.github.com>
2023-05-29 07:32:32 -0600

    Add files via upload

---
+++ adivina_num.c
@@ -0,0 +1,27 @@
+//adivina num
+
+#include<stdio.h>
+
+ int main() {
+ 	
+ 	int numerosecreto = 20;
+ 	int intento;
+ 	
+     printf("adivina el num secreto:");
+     scanf("%d", &intento);
+     
+     if(intento >numerosecreto){
+     	
+	 
+	 printf("el num es muy alto./n");
+	 
+	 }else if(intento <numerosecreto);
+	 printf("el num es muy abajo./n")
+	 }else{
+		printf("le atinasteee./n");
+	
+	}
+	
+ 	return 0;
+ 
+
