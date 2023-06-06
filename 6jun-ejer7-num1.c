f7d5060d4102e7ab89c343286102b8aae486fdca
Mané <131938509+ManeItsHere@users.noreply.github.com>
2023-06-06 10:46:28 -0600

    Add files via upload

---
+++ 6jun-ejer7-num1.c
@@ -0,0 +1,18 @@
+// marlene rm 06-06-23 
+// sin llaves es un gran bucle de 1
+// con llaves se imprimen numeros del 1 al 24
+
+#include <stdio.h>
+
+int main ()
+{
+    int contador = 1;
+
+    while (contador < 25)
+    {
+        printf("%d\n", contador);
+        contador++;
+    }
+
+    return 0;
+}
