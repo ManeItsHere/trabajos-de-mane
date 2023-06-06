76049174f770c9c892c4841460270a9a689905c1
Mané <131938509+ManeItsHere@users.noreply.github.com>
2023-06-06 10:05:12 -0600

    Add files via upload

---
+++ 6jun_ejer4_contador.c
@@ -0,0 +1,14 @@
+//marlene rm 06-06-2023 ejer 4
+
+#include <stdio.h>
+
+int main() {
+    int contador = 1;
+
+    while (contador <= 100) {
+        printf("%d\n", contador);
+        contador++;
+    }
+
+    return 0;
+}
