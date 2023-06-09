65e86f04f6c1eb701444918fa75dd67e02e8212a
Mané <131938509+ManeItsHere@users.noreply.github.com>
2023-06-09 11:06:29 -0600

    Add files via upload

---
+++ 9jun-ejer6.cpp
@@ -0,0 +1,12 @@
+//mane rm 9jun 6
+// imp x abajo de otra con los num
+#include <stdio.h>
+
+int main() {
+   int x = 0;
+   do {
+      printf("x: %d\n", x++);
+   } while (x < 10);
+
+   return 0;
+}
