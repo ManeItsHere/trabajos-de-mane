1dd2edd05a38285fea9b964b6626768344f484ca
Mané <131938509+ManeItsHere@users.noreply.github.com>
2023-06-09 10:22:35 -0600

    Add files via upload

---
+++ 9jun-ejerc1_For.c
@@ -0,0 +1,14 @@
+//mane rm 9ju-n 1
+// for infinito, se para con ctrl c
+
+#include <stdio.h>
+
+int main() {
+    int fila, blancos, cuenta_as;
+    
+    for (;;) {
+        printf("siempre asi, te llamamos siempre\n");
+    
+    
+}
+
