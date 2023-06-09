c94a52499ab720de4ea8d9e0aad4330e237e8671
Mané <131938509+ManeItsHere@users.noreply.github.com>
2023-06-09 10:58:54 -0600

    Add files via upload

---
+++ 9jun-ejer5_ImpAbc.c
@@ -0,0 +1,14 @@
+//mane rm 9jun 5 saludo
+#include <stdio.h>
+
+int main() {
+    char car = 'a';
+
+    do {
+        printf("%c\n", car);
+        car++;
+    } while (car <= 'z');
+
+    return 0;
+}
+
